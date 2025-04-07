52b0f89d9c64c7662f6482cc9aa0aa69ef1de495
tanui-ivy <ivy.tanui@student.moringaschool.com>
2025-04-07 13:33:15 +0300

    Task complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    
+    a, b = 0, 1
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b  
+ 
+    print(f"{fibonacci_sequence}")
+
+
